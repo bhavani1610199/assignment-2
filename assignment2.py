54053de2224f6598faab9866034a38ed7b899485
Bhavani <85815783+bhavani1610199@users.noreply.github.com>
2021-06-13 11:47:58 +0530

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,90 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("30 days 30 hour challenge")
+print('30 days 30 hour challenge')
+
+#Assigning String to Variable:
+
+Hours = "thirty"
+print(Hours)
+
+#Indexing using String:
+
+Days = "Thirty days"
+print(Days[2])
+
+#How to print the particular character from certain text?
+
+Challenge = "I will run "
+print(Challenge[7:10])
+
+#Print the length of Character:
+
+Challenge = "I will win"
+print(len(Challenge))
+
+#Convert String into lower character;
+
+Challenge = "I will win"
+print(Challenge.lower())
+
+#tring Concatenation – Joining two strings
+
+a = "30 Days"
+b = "30 hours"
+c = a + b
+print(c)
+
+#Adding space during concatenation
+
+a = "30 Days"
+b = "30 hours"
+c = a + " " + b
+print(c)
+
+#casefold() - Usage
+
+text = "Thirty days and Thirty hours"
+x = text.casefold()
+print(x)
+
+text = "Thirty days and Thirty hours"
+x = text.capitalize()
+print(x)
+
+text = "Thirty days and Thirty hours"
+x = text.find("i")
+print(x)
+
+text = "Thirty days and Thirty hours"
+x = text.isalpha()
+print(x)
+y="alpahabeticword"
+z=y.isalpha()
+print(z)
+
+text = "Thirty days and Thirty hours"
+x = text.isalnum()
+print(x)
+
+txt = "Return10"
+x = txt.isalnum()
+print(x)
+
+
+
+
