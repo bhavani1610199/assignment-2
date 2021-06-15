23e54ea344b5a77294efbe2418091791587494c2
Bhavani <85815783+bhavani1610199@users.noreply.github.com>
2021-06-15 13:57:06 +0530

    Add files via upload

---
+++ day4ass.py
@@ -0,0 +1,62 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm
+# excerise 1
+
+a = 10
+b = 10
+c = 10
+d = a/10
+e = b*50
+f = c+60
+print("a is divided by 10:",d)
+print("b is mulitipled by 50:",e)
+print("add value c and 60:",f)
+
+#acessing variables
+x = "ab"
+y = "cd"
+z = "ef"
+x2= "gh"
+y2 = "ij"
+print (x)
+print (y)
+print (z)
+print (x2)
+print (y2)
+txt = "ab"
+x = txt.replace("ab","g")
+print (x)
+# exercise 2
+#replacing varibales
+
+#excerise 3
+a = 12
+b = 12.4
+print(int(a))
+print(float(b))
+# int to float
+a = 5
+a = float(a)
+print(float(a))
+b = 12.4
+b = int(b)
+print(int(b))
+
+
+
+
+
